299bc3903aff73c89294e284adf470fcb8e608be
nek0 <neko.rloove@rocketmail.com>
2014-08-15 13:25:33 +0200

    changed profile handling

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -1,13 +1,18 @@
 module Handler.Profile where
 
 import Import
+import Data.Maybe
 
 getProfileR :: UserId -> Handler Html
-getProfileR userId = error "reworking logic" -- do
---  msu <- lookupSession "id"
---  case msu of
---    Just tempUserId -> do
---      userId <- Key $ PersistInt64 $ fromIntegral tempUserId
---      userMedia <- runDB $ selectList [MediumOwner ==. userId] [Desc MediumTime]
---      defaultLayout $ do
---        $(widgetFile "profile")
+getProfileR user = do
+  owner <- runDB $ get user
+  ownerName <- lift $ pure $ userName $ fromJust owner
+  userAlbums <- runDB $ selectList [AlbumOwner ==. user] [Desc AlbumTitle]
+  recentMedia <- (runDB $ selectList [MediumOwner ==. user] [Desc MediumTime])
+  msu <- lookupSession "userId"
+  case msu of
+    Just tempUserId -> do
+      userId <- lift $ pure $ getUserIdFromText tempUserId
+      presence <- lift $ pure $ (Key userId) == user
+      defaultLayout $ do
+        $(widgetFile "profile")
