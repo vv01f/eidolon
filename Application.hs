96a7a28a42b6fce3b019ac0f7e8125099ab4e4cc
nek0 <neko.rloove@rocketmail.com>
2014-08-17 22:59:17 +0200

    preparing albumsite

--- Application.hs
+++ Application.hs
@@ -34,6 +34,7 @@ import Handler.Activate
 import Handler.Profile
 import Handler.Upload
 import Handler.NewAlbum
+import Handler.Album
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
