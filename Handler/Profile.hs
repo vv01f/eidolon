2c99c0fe6ba2d64839322e4a09afae20f6690821
nek0 <neko.rloove@rocketmail.com>
2014-08-17 22:58:33 +0200

    profilesite working

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -7,12 +7,15 @@ getProfileR :: UserId -> Handler Html
 getProfileR user = do
   owner <- runDB $ get user
   ownerName <- lift $ pure $ userName $ fromJust owner
-  userAlbums <- runDB $ selectList [AlbumOwner ==. user] [Desc AlbumTitle]
+  userAlbs <- runDB $ selectList [AlbumOwner ==. user] [Desc AlbumTitle]
   recentMedia <- (runDB $ selectList [MediumOwner ==. user] [Desc MediumTime])
   msu <- lookupSession "userId"
   case msu of
     Just tempUserId -> do
       userId <- lift $ pure $ getUserIdFromText tempUserId
       presence <- lift $ pure $ userId == user
+      defaultLayout $ do
+        $(widgetFile "profile")
+    Nothing ->
       defaultLayout $ do
         $(widgetFile "profile")
