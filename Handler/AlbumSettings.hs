9d33fea611607a4ce85c4e41669eced7000a4f04
nek0 <neko.rloove@rocketmail.com>
2014-08-30 00:19:52 +0200

    albums now can be deleted

--- Handler/AlbumSettings.hs
+++ Handler/AlbumSettings.hs
@@ -1,6 +1,9 @@
 module Handler.AlbumSettings where
 
 import Import
+import qualified Data.Text as T
+import System.Directory
+import System.FilePath
 
 getAlbumSettingsR :: AlbumId -> Handler Html
 getAlbumSettingsR albumId = do
@@ -79,3 +82,63 @@ albumSettingsForm album albumId = renderDivs $ Album
     media = do
       entities <- runDB $ selectList [MediumAlbum ==. albumId] [Desc MediumTitle]
       optionsPairs $ map (\med -> (mediumTitle $ entityVal med, mediumPath (entityVal med))) entities
+
+getAlbumDeleteR :: AlbumId -> Handler Html
+getAlbumDeleteR albumId = do
+  tempAlbum <- runDB $ get albumId
+  case tempAlbum of
+    Just album -> do
+      ownerId <- return $ albumOwner album
+      owner <- runDB $ getJust ownerId
+      ownerName <- return $ userName owner
+      msu <- lookupSession "userId"
+      case msu of
+        Just tempUserId -> do
+          userId <- return $ getUserIdFromText tempUserId
+          presence <- return (userId == ownerId)
+          case presence of
+            True -> do
+              defaultLayout $ do
+                $(widgetFile "albumDelete")
+            False -> do
+              setMessage "You must own this album to delete it"
+              redirect $ AlbumR albumId
+        Nothing -> do
+          setMessage "You must be logged in to delete albums"
+          redirect $ LoginR
+    Nothing -> do
+      setMessage "This album does not exist"
+      redirect $ HomeR
+
+postAlbumDeleteR :: AlbumId -> Handler Html
+postAlbumDeleteR albumId = do
+  tempAlbum <- runDB $ get albumId
+  case tempAlbum of
+    Just album -> do
+      ownerId <- return $ albumOwner album
+      owner <- runDB $ getJust ownerId
+      ownerName <- return $ userName owner
+      msu <- lookupSession "userId"
+      case msu of
+        Just tempUserId -> do
+          userId <- return $ getUserIdFromText tempUserId
+          presence <- return (userId == ownerId)
+          case presence of
+            True -> do
+              confirm <- lookupPostParam "confirm"
+              case confirm of
+                Just "confirm" -> do
+                  mapM (\a -> runDB $ delete a) (albumContent album)
+                  runDB $ delete albumId
+                  liftIO $ removeDirectoryRecursive $ "static" </> "data" </> (T.unpack $ extractKey userId) </> (T.unpack $ extractKey albumId)
+                  setMessage "Album deleted succesfully"
+                  redirect $ HomeR
+            False -> do
+              setMessage "You must own this album to delete it"
+              redirect $ AlbumR albumId
+        Nothing -> do
+          setMessage "You must be logged in to delete albums"
+          redirect $ LoginR
+    Nothing -> do
+      setMessage "This album does not exist"
+      redirect $ HomeR
