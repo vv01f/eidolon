fec1b02780893139a366bce528e510b85de2e087
nek0 <neko.rloove@rocketmail.com>
2014-09-05 20:40:12 +0200

    Profile settings and user deletion

--- Application.hs
+++ Application.hs
@@ -39,6 +39,8 @@ import Handler.Medium
 import Handler.AlbumSettings
 import Handler.MediumSettings
 import Handler.Reactivate
+import Handler.ProfileSettings
+import Handler.ProfileDelete
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
