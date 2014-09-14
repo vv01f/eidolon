57610a8b996af5dc51e2f148e8b02d509f816f7d
nek0 <neko.rloove@rocketmail.com>
2014-09-14 05:03:13 +0200

    working with tags

---
+++ Handler/Tag.hs
@@ -0,0 +1,16 @@
+module Handler.Tag where
+
+import Import
+import Data.Maybe
+
+getTagR :: Text -> Handler Html
+getTagR tag = do
+  tempMedia <- runDB $ selectList [] [Desc MediumTitle]
+  almostMedia <- mapM (\a -> do
+    case tag `elem` (mediumTags $ entityVal a) of
+      True -> return (Just a)
+      False -> return Nothing
+      ) tempMedia
+  media <- return $ removeItem Nothing almostMedia
+  defaultLayout $ do
+    $(widgetFile "tagMedia")
