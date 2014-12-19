b8e8a0c334f0be5e72d5758d59300af09f95e0db
nek0 <nek0@chelnok.de>
2014-12-19 22:37:32 +0100

    fixed order of tag media

--- Handler/Tag.hs
+++ Handler/Tag.hs
@@ -6,7 +6,7 @@ import Data.Maybe
 
 getTagR :: Text -> Handler Html
 getTagR tag = do
-  tempMedia <- runDB $ selectList [] [Asc MediumTitle]
+  tempMedia <- runDB $ selectList [] [Desc MediumTitle]
   almostMedia <- mapM (\a -> do
     case tag `elem` (mediumTags $ entityVal a) of
       True -> return (Just a)
