4a71f570845aace49535cb78f95fa58147070fc6
nek0 <nek0@chelnok.de>
2015-01-18 21:25:49 +0100

    error by license

--- Handler/Activate.hs
+++ Handler/Activate.hs
@@ -13,6 +13,7 @@
 --
 --  You should have received a copy of the GNU Affero General Public License
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.module Handler.Activate where
+module Handler.Activate where
 
 import Import as I hiding (returnJson)
 import Data.Text
